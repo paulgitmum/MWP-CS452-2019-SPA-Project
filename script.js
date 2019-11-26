ebe2f33ed8cf640a954f840a3119a7b8ae5bd329
paulgitmum <54817437+paulgitmum@users.noreply.github.com>
2019-11-26 15:55:53 -0600

    my login

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// your code here
+// your code // your code here
+
+window.onload = function(){
+
+
+    // login window 
+    let welcome = ` User Name <input type = "text"  placeholder="mwp"/><br><br>
+Password <input type = "text" placeholder="123"/><br><br>
+  <button id = "btn" class="btn btn-primary" >Login</button>
+
+`;
+
+// animation window
+
+// let animation = ` <h2>Welcome all from Fairfeild,IA,US!</h2>
+// <textarea rows="20" cols="50"></textarea><br><br>
+// <button id = "btn">Refresh Animation</button>
+// <button id = "btn">Logout</button>
+
+// `;
+
+document.getElementById("outlet").innerHTML= welcome;
+document.getElementById("outlet").innerHTML= animation;
+lshfsh
+
+
+}
