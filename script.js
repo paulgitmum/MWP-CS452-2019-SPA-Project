566ea9cf57e02d602ba3f23d16e5cfe0da1cf473
paulgitmum <54817437+paulgitmum@users.noreply.github.com>
2019-11-27 01:33:55 -0600

    First day

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 // your code // your code here
 
-window.onload = function(){
+window.onload = function () {
 
 
     // login window 
-    let welcome = ` User Name <input type = "text"  placeholder="mwp"/><br><br>
+    let welcome = `
+     User Name <input type = "text"  placeholder="mwp"/><br><br>
 Password <input type = "text" placeholder="123"/><br><br>
   <button id = "btn" class="btn btn-primary" >Login</button>
-
 `;
 
-// animation window
+    // animation window
 
-// let animation = ` <h2>Welcome all from Fairfeild,IA,US!</h2>
-// <textarea rows="20" cols="50"></textarea><br><br>
-// <button id = "btn">Refresh Animation</button>
-// <button id = "btn">Logout</button>
+    let animation = ` <h2 id = "new" ></h2> 
+<textarea rows="20" cols="50"></textarea><br><br>
+<button id = "btn">Refresh Animation</button>
+<button id = "button">Logout</button>
+`;
 
-// `;
+    // 
+    document.querySelector("#outlet").innerHTML = animation;
+    let input = document.querySelector("#outlet");
+    input.innerHTML = welcome;
 
-document.getElementById("outlet").innerHTML= welcome;
-document.getElementById("outlet").innerHTML= animation;
-lshfsh
+    let nextPage = document.querySelector("#btn");
+    nextPage.addEventListener("click", logIn)
 
+    function logIn() {
 
+        input.innerHTML = animation;
+        backTo();
+
+    }
+
+    function display() {
+        input.innerHTML = welcome;
+    }
+
+    
+    function backTo() {
+        const out = document.querySelector("#button");
+        out.addEventListener("click", display);
+
+    }
+
+    
 }
